17e1132b4c2921954f2b08d6c913f5853d406354
Tiago Cordeiro <tiagoezc@hotmail.com>
2023-07-29 20:43:02 -0300

    Initial SellersController impl + Layout navBar modifications + SellersView Preview

---
+++ SalesWeb/Controllers/SellersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWeb.Controllers {
+    public class SellersController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
